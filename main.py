6fa566235ad946d9ba9153f6fc5d5194ffefa8ac
kalyan2003 <pavan.chimmiri2003@gmail.com>
2025-03-27 15:08:34 +0530

    Implemented the test case -5

--- main.py
+++ main.py
@@ -1,26 +1,29 @@
 import random
 
+
 class SnakeLadder:
     def __init__(self):
         self.position_player1 = 0
+        self.dice_roll_count = 0
 
-
-    def roll_dice(self):
-        return random.randint(1,6)
+    @staticmethod
+    def roll_dice():
+        return random.randint(1, 6)
 
     def play_turn(self):
-        dice_value = self.roll_dice()
-        option = random.choice(["No play", "Ladder", "Snake"])
+        dice_value = SnakeLadder.roll_dice()
+        self.dice_roll_count += 1
+        option = random.choice(["No Play", "Ladder", "Snake"])
 
         if option == "No Play":
             pass
         elif option == "Ladder":
-            self.position_player1 += dice_value
+            if self.position_player1 + dice_value <= 100:
+                self.position_player1 += dice_value
         elif option == "Snake":
             self.position_player1 -= dice_value
             if self.position_player1 < 0:
                 self.position_player1 = 0
-
 
         print(f"Dice rolled: {dice_value}, Option: {option}, New Position: {self.position_player1}")
 
@@ -28,7 +31,9 @@ class SnakeLadder:
         while self.position_player1 < 100:
             self.play_turn()
 
+        print(f"\nGame Over! Total dice rolls: {self.dice_roll_count}")
 
+
+# Start the game
 game = SnakeLadder()
-
 game.play_game()
