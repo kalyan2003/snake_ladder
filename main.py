e6eed1573c68f97215ffdf77564b6671e70487fd
kalyan2003 <pavan.chimmiri2003@gmail.com>
2025-03-27 14:34:23 +0530

    Implemented the use-case3

--- main.py
+++ main.py
@@ -2,9 +2,28 @@ import random
 
 class SnakeLadder:
     def __init__(self):
-        self.position = 0
+        self.position_player1 = 0
 
     @staticmethod
     def roll_dice():
         return random.randint(1,6)
 
+    def play_turn(self):
+        dice_value = self.roll_dice()
+        option = random.choice(["No play", "Ladder", "Snake"])
+
+        if option == "No Play":
+            pass
+        elif option == "Ladder":
+            self.position_player1 += dice_value
+        elif option == "Snake":
+            self.position_player1 -= dice_value
+            if self.position_player1 < 0:
+                self.position_player1 = 0
+
+
+        print(f"Dice rolled: {dice_value}, Option: {option}, New Position: {self.position_player1}")
+
+game = SnakeLadder()
+
+game.play_turn()
